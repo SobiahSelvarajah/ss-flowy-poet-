3e6cab265dbe8c07d5139a2350ed720c00656ff9
Sobiah Selvarajah <sobiahselvarajah@hotmail.co.uk>
2024-04-27 13:14:41 +0100

    app: routing added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Homepage from './pages/homepage/Homepage'
+import About from './pages/about/About'
+import Gallery from './pages/gallery/Gallery'
+import Dictionary from './pages/dictionary/Dictionary'
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Homepage/>}/>
+        <Route path='about' element={<About/>}/>
+        <Route path='gallery' element={<Gallery/>}/>
+        <Route path='dictionary' element={<Dictionary/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
